80fa203984a1180ea70023c4494241b6b2e1bfce
randomplayer2002 <amith.hathwar02@gmail.com>
2023-02-12 10:13:43 +0530

    Done

--- CNOE/CaesarCipher.java
+++ CNOE/CaesarCipher.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;   
        
-public class CaesarCipherExample   
+public class CaesarCipher  
 {   
     public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";   
       
