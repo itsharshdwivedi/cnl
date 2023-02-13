139ee03756e49ac01b3c03a310af0e86c98a84b7
randomplayer2002 <amith.hathwar02@gmail.com>
2023-02-13 21:46:35 +0530

    Done

--- Program 10: UDP CLIENT AND SERVER/UDPSender.java
+++ Program 10: UDP CLIENT AND SERVER/UDPSender.java
@@ -12,7 +12,7 @@ public class UDPSender
 		{
 			String msg = scanner.nextLine();  
 			InetAddress ip = InetAddress.getByName("127.0.0.1");  
-		DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.length(), ip, 3000);  
+			DatagramPacket dp = new DatagramPacket(msg.getBytes(), msg.length(), ip, 3000);  
 			ds.send(dp);  
 		}  
 	}  
